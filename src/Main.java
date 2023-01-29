978503eede6a2f66909645fa834b4598f69d24cc
Ilya_S <wermach95@gmail.com>
2023-01-29 23:31:27 +0400

    Merge pull request #1 from IlyaShibaev/bugfix
    test

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Cat barsik = new Cat("vasilek_44", 6, "orange", 6.5);
+        Cat barsik = new Cat("vasilek", 6, "orange", 6.5);
         Cat puffistic = new Cat();
+        Cat lovely = new Cat("branch", 6, "orange", 6.0);
     }
 }
