e62f91bac661b9ce5ce800f1a64f37faad3500f7
Ilya_S <wermach95@gmail.com>
2023-05-24 23:50:00 +0400

    message

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Cat barsik = new Cat("vasilek", 6, "orange", 6.5);
+        Cat barsik = new Cat("vasilek_1", 6, "orange", 6.5);
         Cat puffistic = new Cat();
         Cat lovely = new Cat("branch_2", 6, "orange", 6.0);
     }
